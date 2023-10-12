dea1a82f1435a2ee3a03a5dd5563bbd4008dd340
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-10-12 15:47:10 +0100

    Delete migration files

--- prisma/migrations/20231012142016_dev/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Movie" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-    "overview" TEXT NOT NULL,
-
-    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
-);
