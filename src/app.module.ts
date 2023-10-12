d4ac65b8cb32fc041a160107a292ce82678b809e
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-10-12 13:17:18 +0100

    .env update, prisma integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
 import { QrModule } from './qr/qr.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [MovieModule, QrModule, ConfigModule.forRoot()],
+  imports: [MovieModule, QrModule, ConfigModule.forRoot(), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
   // imports: [ConfigModule.forRoot()],
