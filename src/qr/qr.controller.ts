5d99a661ef33ac59e8853371d0b4989f38b9b8f5
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-10-13 20:42:45 +0100

    QR controller

--- src/qr/qr.controller.ts
+++ src/qr/qr.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { QrService } from './qr.service';
+import { Response } from 'express';
+import * as qr from 'qrcode';
 
 @Controller('qr')
 export class QrController {
   constructor(private readonly qrService: QrService) {}
 
   @Get()
-  getQRCodes() {
-    return this.qrService.getQRCodes();
-  }
+  async getQRCode(@Res() res: Response) {
+    const movieUrl = `https://qr-code-fe-sigma.vercel.app/movies`;
+    const qrCode = await qr.toDataURL(movieUrl);
 
-  @Get('random')
-  getRa() {
-    return this.qrService.getRandomMovies();
+    res.setHeader('Content-Type', 'image/png');
+    res.send(Buffer.from(qrCode.split(',')[1], 'base64'));
   }
 }
